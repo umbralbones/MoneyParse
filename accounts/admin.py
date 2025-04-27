1eaf2d36c4c5eedb3465090e854376e1aba784ed
lilychisholm <lsc112003@gmail.com>
2025-04-27 19:47:34 -0400

    added user control to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(BaseUserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+
+    list_display = ('email', 'is_staff', 'is_active')
+    list_filter  = ('is_staff', 'is_active')
+    ordering     = ('email',)
+    search_fields = ('email',)
+
+    fieldsets = (
+        (None,            {'fields': ('email', 'password')}),
+        ('Permissions',   {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login','date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_active', 'is_staff'),
+        }),
+    )
